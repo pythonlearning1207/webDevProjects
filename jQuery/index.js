f24b232ad9f7811be8c4c10d369f1ce303b21b90
pythonlearning1207 <47767461+pythonlearning1207@users.noreply.github.com>
2024-01-22 20:22:16 -0800

    finished Section19

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,2 +1,24 @@
 console.log($("h1").css("color"));
 $("a").attr("href", "https://www.yahoo.com/");
+
+
+$("h1").html("<em>Hello!</em>");
+
+$("h1").addClass("big")
+/*$("body").keypress(function(event){
+    $("h1").html(event.key);
+})*/
+
+$("h1").mouseover(function(){
+    $("h1").css("color", "purple");
+})
+
+$("h1").mouseout(function(){
+    $("h1").css("color", "black");
+})
+
+$("h1").before("<button>New</button>");
+
+$("button").click(function(){
+    $("h1").animate({margin: 20%});
+})
